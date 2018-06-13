1bf38cb766a65175fc17c8d0db9e26937667123d
ben stear <benstear@yahoo.com>
2018-06-12 20:03:05 -0400

    Update VectorAdd_serial.py

--- VectorAdd_serial.py
+++ VectorAdd_serial.py
@@ -2,30 +2,16 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Jun 10 18:47:28 2018
-
-@author: dawnstear
+@author: benstear
 """
 
-# graph theory coding
-
-#
 #    serial vs paralell computing
-#   parallelizable or not? searching--yes, have 10 different workers each search a line
-#find nth fibonacci number? no, each nth number depends on the previous number 
-
-
-# faster code always means more efficient?
-
 # if using cuda, file extension is .cu ,, nvcc = nvidia's compiler
 # cuda architecture consists of threads, blocks and grids
-
-# how does quadcore handle the problem of gpu computing, wherefibonacci has trouble??
-
 # gpu invented bc each pixel can be computed independently, for transformations
 # gpu's can have thousands of cores
 # cpus = general, very fast , gpus = high bandwidth
 # machine learning/DL also uses highly parallel massive matrix ops
-
 # AWS is amazons cloud computing platform, google cloud is cheaper though
 
 
@@ -43,7 +29,6 @@ def main():
         A = np.ones(N,dtype=np.float32)  
         B = np.ones(N,dtype=np.float32)
         C = np.zeros(N,dtype=np.float32)
-        
         
         start = timer()
         VectorAdd(A,B,C)
